5b72f7d77543133212d4a53a6bd777a41038745d
Michał Ciesielski <majalcmaj@gmail.com>
2022-10-15 02:00:05 +0200

    Bit of cleanup

---
+++ functions/clients/get-yrno-forecast/src/app.js
@@ -0,0 +1,36 @@
+import fetchForecast from "./forecast-fetcher.js";
+
+function parseReport(rawReport) {
+  const timeseries = rawReport.properties.timeseries;
+  const timestamps = timeseries.map((measure) => measure.time);
+  const windSpeeds = timeseries.map(
+    (measure) => measure.data.instant.details.wind_speed
+  );
+  const windDirections = timeseries.map(
+    (measure) => measure.data.instant.details.wind_from_direction
+  );
+  return { timestamps, windSpeeds, windDirections };
+}
+
+async function getYrNoForecast(fetchForecastFn) {
+  try {
+    const rawReport = (await fetchForecastFn()).data;
+    const parsedReport = parseReport(rawReport);
+    return { forecast: parsedReport };
+  } catch (error) {
+    console.error(error);
+    return { error: error };
+  }
+}
+
+/**
+ * @param {Object} event - Input event to the Lambda function
+ * @param {Object} context - Lambda Context runtime methods and attributes
+ *
+ * @returns {Object} object - Object containing the parsed forecast
+ */
+async function lambdaHandler(event, context) {
+  return getYrNoForecast(fetchForecast);
+};
+
+export { lambdaHandler, getYrNoForecast };
--- functions/clients/get-yrno-forecast/src/forecast-fetcher.spec.js
+++ functions/clients/get-yrno-forecast/src/forecast-fetcher.spec.js
@@ -1,12 +1,12 @@
-import fetchForecast from '../../forecast-fetcher.js';
-import {expect} from 'chai';
+import fetchForecast from './forecast-fetcher.js';
+import {expect, assert} from 'chai';
 
 describe('Test request', function() {
   it('Data has an expected shape', async () => {
     const result = await fetchForecast();
 
     expect(result.status).to.be.eq(200);
-
+    assert.fail("Implement me!");
   });
 })
 
